61a8001574ed1ce43823c75376264ceb2337957d
pnguye92 <pnguye92@uwo.ca>
2025-05-04 22:37:24 -0400

    Edit interative elements

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// You can add contact form submission, dark mode toggle, etc. here
-console.log("Welcome to Brielle's portfolio!");
+document.getElementById('theme-toggle').addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  const icon = document.getElementById('theme-toggle').querySelector('i');
+  icon.classList.toggle('fa-moon');
+  icon.classList.toggle('fa-sun');
+});
