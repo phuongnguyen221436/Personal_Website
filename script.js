cbd919bc45cfbd26306f9f7ae1e9441f0525665f
pnguye92 <pnguye92@uwo.ca>
2025-05-04 22:52:31 -0400

    Fix up

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.getElementById('theme-toggle').addEventListener('click', () => {
+const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
-  const icon = document.getElementById('theme-toggle').querySelector('i');
-  icon.classList.toggle('fa-moon');
-  icon.classList.toggle('fa-sun');
 });
