3343f9e5cb8555a88cf1673b50b2e3deb57b39d4
pnguye92 <pnguye92@uwo.ca>
2025-09-18 16:29:25 -0400

    fix

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const toggleButton = document.getElementById('dark-mode-toggle');
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+var typed = new Typed("#typing-text", {
+  strings: [
+    "Aspiring Software Engineer",
+    "Product Manager in the making",
+    "Passionate about impactful digital experiences"
+  ],
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 1500,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|"
+});
